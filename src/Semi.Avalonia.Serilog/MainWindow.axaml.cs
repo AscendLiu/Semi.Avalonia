01195bba5986186d281f92a99af930ff60b0cf78
sliu <sliu@xatoll.com>
2024-09-24 18:41:53 +0800

    1

---
+++ src/Semi.Avalonia.Serilog/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Semi.Avalonia.Serilog;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
